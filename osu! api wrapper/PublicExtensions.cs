a79540077ebc374a7bd8e854e1ba0caf12a77e0e
HoLLy-HaCKeR <dario.db@hotmail.com>
2016-07-22 17:35:33 +0200

    Add extension method for Mods

---
+++ osu! api wrapper/PublicExtensions.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace osu_api_wrapper
+{
+    public static class PublicExtensions
+    {
+        public static Mods[] GetArray(this Mods mods)
+        {
+            List<Mods> modsList = new List<Mods>();
+
+            foreach (Mods value in Enum.GetValues(typeof(Mods)))
+                if (mods.HasFlag(value))
+                    modsList.Add(value);
+
+            return modsList.ToArray();
+        }
+    }
+}
